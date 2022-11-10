626bf3c75b367d98f4218ca5d346453f1d076f03
kewcow <kevjhoss@gmail.com>
2022-11-10 00:24:41 -0300

    version 1.0

---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+    return array(
+        "driver"   => "mysql",
+        "host"     => "containers-us-west-83.railway.app:7463",
+        "user"     => "root",
+        "pass"     => "skekvhVcuAy9vLFtoyP0",
+        "database" => "railway",
+        "charset"  => "utf8"
+
+    );
+?>
