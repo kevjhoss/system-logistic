b9a8d89a63d1546fa26349b3fb47bd9a307c2194
kewcow <kevjhoss@gmail.com>
2022-11-18 21:44:05 -0300

    version 3.7

---
+++ view/homeView.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang='es' dir='ltr'>
+  <head>
+    <?php include './view/meta.php';?>
+    <title>Logistica</title>
+  </head>
+  <body>
+    <?php include './view/navegation.php';?>
+  </body>
+</html>
---
+++ view/navegation.php
@@ -0,0 +1,22 @@
+<?php include "./view/components/Icons.php";?>
+<nav>
+  <?php echo Icons('logo', '3em', 'hsl(0,0%,100%)')?>
+  <ul>
+    <li>
+      <?php echo Icons('home', '1em', 'hsl(0,0%,100%)')?>
+      Panel
+    </li>
+    <li>
+      Envio
+      <?php echo Icons('arrow-down', '1em', 'hsl(0,0%,100%)')?>
+    </li>
+    <li>
+      Mis envios
+      <?php echo Icons('arrow-down', '1em', 'hsl(0,0%,100%)')?>
+    </li>
+    <li>
+      <?php echo Icons('account', '1em', 'hsl(0,0%,100%)')?>
+      Cuenta
+    </li>
+  </ul>
+</nav>
